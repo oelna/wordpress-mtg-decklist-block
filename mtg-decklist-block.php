27fe6ba50dddaba15bdbb37c373356ba39eeb721
Arno Richter <arno.richter+github@gmail.com>
2026-02-27 09:54:21 +0100

    Add files via upload

--- mtg-decklist-block.php
+++ mtg-decklist-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MTG Decklist Block
  * Description: Gutenberg block that turns Magic: The Gathering decklists into a formatted, linked table with grouping, Scryfall enrichment, and clipboard copy.
- * Version: 1.2.0
+ * Version: 1.3.3
  * Update URI: https://github.com/oelna/wordpress-mtg-decklist-block
  * Author: Arno Richter
  * License: MIT
@@ -816,7 +816,7 @@ function mtgdl_github_latest_release_cached() {
 			$url = isset($asset['browser_download_url']) ? (string)$asset['browser_download_url'] : '';
 			if ($url && preg_match('/\\.zip$/i', $name)) {
 				// Optional: enforce a specific filename
-				// if ($name !== 'mtg-decklist-block.zip') { continue; }
+				if ($name !== 'mtg-decklist-block.zip') { continue; }
 				$package = $url;
 				break;
 			}
@@ -833,165 +833,3 @@ function mtgdl_github_latest_release_cached() {
 
 	return $result;
 }
-
-
-/*
-// Auto-updates from Github, via: https://github.com/rudrastyh/misha-update-checker
-if( ! class_exists( 'mtgdlUpdateCheck' ) ) {
-
-	class mtgdlUpdateCheck{
-
-		public $plugin_slug;
-		public $version;
-		public $cache_key;
-		public $cache_allowed;
-
-		public function __construct() {
-
-			$this->plugin_slug = plugin_basename( __DIR__ );
-			$this->version = '1.0';
-			$this->cache_key = 'misha_custom_upd';
-			$this->cache_allowed = false;
-
-			add_filter( 'plugins_api', array( $this, 'info' ), 20, 3 );
-			add_filter( 'site_transient_update_plugins', array( $this, 'update' ) );
-			add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
-
-		}
-
-		public function request(){
-
-			$remote = get_transient( $this->cache_key );
-
-			if( false === $remote || ! $this->cache_allowed ) {
-
-				$remote = wp_remote_get(
-					'https://rudrastyh.com/wp-content/uploads/updater/info.json',
-					array(
-						'timeout' => 10,
-						'headers' => array(
-							'Accept' => 'application/json'
-						)
-					)
-				);
-
-				if(
-					is_wp_error( $remote )
-					|| 200 !== wp_remote_retrieve_response_code( $remote )
-					|| empty( wp_remote_retrieve_body( $remote ) )
-				) {
-					return false;
-				}
-
-				set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
-
-			}
-
-			$remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-			return $remote;
-
-		}
-
-
-		function info( $res, $action, $args ) {
-
-			// print_r( $action );
-			// print_r( $args );
-
-			// do nothing if you're not getting plugin information right now
-			if( 'plugin_information' !== $action ) {
-				return $res;
-			}
-
-			// do nothing if it is not our plugin
-			if( $this->plugin_slug !== $args->slug ) {
-				return $res;
-			}
-
-			// get updates
-			$remote = $this->request();
-
-			if( ! $remote ) {
-				return $res;
-			}
-
-			$res = new stdClass();
-
-			$res->name = $remote->name;
-			$res->slug = $remote->slug;
-			$res->version = $remote->version;
-			$res->tested = $remote->tested;
-			$res->requires = $remote->requires;
-			$res->author = $remote->author;
-			$res->author_profile = $remote->author_profile;
-			$res->download_link = $remote->download_url;
-			$res->trunk = $remote->download_url;
-			$res->requires_php = $remote->requires_php;
-			$res->last_updated = $remote->last_updated;
-
-			$res->sections = array(
-				'description' => $remote->sections->description,
-				'installation' => $remote->sections->installation,
-				'changelog' => $remote->sections->changelog
-			);
-
-			if( ! empty( $remote->banners ) ) {
-				$res->banners = array(
-					'low' => $remote->banners->low,
-					'high' => $remote->banners->high
-				);
-			}
-
-			return $res;
-
-		}
-
-		public function update( $transient ) {
-
-			if ( empty($transient->checked ) ) {
-				return $transient;
-			}
-
-			$remote = $this->request();
-
-			if(
-				$remote
-				&& version_compare( $this->version, $remote->version, '<' )
-				&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' )
-				&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
-			) {
-				$res = new stdClass();
-				$res->slug = $this->plugin_slug;
-				$res->plugin = plugin_basename( __FILE__ ); // misha-update-plugin/misha-update-plugin.php
-				$res->new_version = $remote->version;
-				$res->tested = $remote->tested;
-				$res->package = $remote->download_url;
-
-				$transient->response[ $res->plugin ] = $res;
-
-	    }
-
-			return $transient;
-
-		}
-
-		public function purge( $upgrader, $options ){
-
-			if (
-				$this->cache_allowed
-				&& 'update' === $options['action']
-				&& 'plugin' === $options[ 'type' ]
-			) {
-				// just clean the cache when new plugin version is installed
-				delete_transient( $this->cache_key );
-			}
-
-		}
-
-
-	}
-
-	new mtgdlUpdateCheck();
-}
-*/
